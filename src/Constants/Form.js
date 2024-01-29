f686a77258b73a8ef63282bb610eb766816c0181
jkavod <ubongjosevv@gmail.com>
2024-01-29 12:25:11 +0100

    updated

--- src/Constants/Form.js
+++ src/Constants/Form.js
@@ -1,103 +1,89 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+const apiKey =
+  "EAANARNFNq1gBOyzvSsGHg7ej3ZAkZBwdcqSZAt1tt2duazEcFX0I411JLce0ZCSCKwcuiLA1H1k8BdbW4jfrOEKQppooZBd4xfVQnhWBeZAqhmH35ZC6rxKzn9nYKBQW0EEtl6apfZBdwUzAD66dKsfVM05nF5vq2zETkePQgFyb158KXmKR9ZBeZC2SFM0CjlTYlNWoKQ3vLsmOqpZCrebuF1lrIz7gzpyYGzX";
+
 export default function Form() {
- const [phoneNumber, setPhoneNumber] = useState("");
- const [error, setError] = useState(null);
- const [success, setSuccess] = useState(null);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
- const handleSubmit = async (e) => {
-   e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-   try {
-     const encodedPhoneNumber = encodeURIComponent(phoneNumber);
+    try {
+      // Save data to the Firebase Realtime Database
+      const dbResponse = await fetch(
+        "https://yiieldy-web-default-rtdb.firebaseio.com/yiieldy_wait_lists_users.json",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ Number: phoneNumber }),
+        }
+      );
 
-     // Check if the phone number is already in the database
-     const checkResponse = await fetch(
-       `https://yiieldy-web-default-rtdb.firebaseio.com/yiieldy_wait_lists_users.json?orderBy="Number"&equalTo="${encodedPhoneNumber}"`,
-       {
-         method: "GET",
-       }
-     );
+      // Check if data saved successfully to the database
+      if (!dbResponse.ok) {
+        throw new Error("Error saving data to the database.");
+      }
 
-     if (!checkResponse.ok) {
-       throw new Error("Error checking the database.");
-     }
-
-     const checkData = await checkResponse.json();
-
-     if (Object.keys(checkData).length > 0) {
-       setError("This phone number is already registered.");
-       setSuccess(null);
-       return;
-     }
-
-     // Save data to the Firebase Realtime Database
-     const dbResponse = await fetch(
-       "https://yiieldy-web-default-rtdb.firebaseio.com/yiieldy_wait_lists_users.json",
-       {
-         method: "POST",
-         headers: {
-           "Content-Type": "application/json",
-         },
-         body: JSON.stringify({ Number: phoneNumber }),
-       }
-     );
-
-     if (!dbResponse.ok) {
-       throw new Error("Error saving data to the database.");
-     }
-
-
-        // Make API request to Facebook Graph API
-        const response = await axios.post(
-          "https://graph.facebook.com/v18.0/203864902816928/messages",
-          {
-            messaging_product: "whatsapp",
-            to: `whatsapp.${phoneNumber}`,
-            type: "template",
-            template: {
-              name: "yiieldy_wait_lists_users",
-              language: {
-                code: "en",
-              },
+      // Make API request to Facebook Graph API
+      const response = await axios.post(
+        "https://graph.facebook.com/v18.0/203864902816928/messages",
+        {
+          messaging_product: "whatsapp",
+          to: `whatsapp.${phoneNumber}`, // Use the phone number from the input
+          type: "template",
+          template: {
+            name: "yiieldy_wait_lists_users",
+            language: {
+              code: "en",
             },
           },
-          {
-            headers: {
-              Authorization: "Bearer EAANARNFNq1gBOyzvSsGHg7ej3ZAkZBwdcqSZAt1tt2duazEcFX0I411JLce0ZCSCKwcuiLA1H1k8BdbW4jfrOEKQppooZBd4xfVQnhWBeZAqhmH35ZC6rxKzn9nYKBQW0EEtl6apfZBdwUzAD66dKsfVM05nF5vq2zETkePQgFyb158KXmKR9ZBeZC2SFM0CjlTYlNWoKQ3vLsmOqpZCrebuF1lrIz7gzpyYGzX",
-              "Content-Type": "application/json",
-            },
-          }
-        );
-
-        // Check if the Facebook API request was successful
-        if (
-          response.status === 200 &&
-          response.data &&
-          response.data.messages[0].status === "sent"
-        ) {
-          setSuccess("Message sent successfully!");
-          setError(null);
-        } else {
-          setError("Error sending message. Please try again.");
-          setSuccess(null);
-          console.error("Facebook API error:", response.data);
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+          },
         }
+      );
 
-        setPhoneNumber(""); // Clear the phone number input
-      } catch (error) {
-       setError("An unexpected error occurred. Please try again.");
-       setSuccess(null);
-       console.error(error);
-     }
-   };
+      // Check if the Facebook API request was successful
+      // if (
+      //   response.status === 200 &&
+      //   response.data &&
+      //   response.data.messages[0].status === "sent"
+      // ) {
+      //   setSuccess("Message sent successfully!");
+      //   setError(null); // Clear any previous error messages
+      // } else {
+      //   setError("Error sending message. Please try again.");
+      //   setSuccess(null); // Clear any previous success messages
+      //   console.error("Facebook API error:", response.data);
+      // }
+
+      // console.log(response);
+
+      setPhoneNumber(""); // Clear the phone number input
+   
+   } 
+   catch (error) {
+    //   // Handle other errors
+    //   setError("An unexpected error occurred. Please try again.");
+    //   setSuccess(null); // Clear any previous success messages
+    //   console.error(error);
+    }
+  };
 
   return (
     <div>
       <div className="text-center">
-        {error && <div className="text-red-800 text-left">{error}</div>}
-        {success && <div className="text-green text-left">{success}</div>}
+        {error && <div className="text-red-500">{error}</div>}
+        {success && <div className="text-green">{success}</div>}
 
         <form
           className="flex flex-col items-center w-full mb-4 md:flex-row"
@@ -105,7 +91,7 @@ export default function Form() {
           method="POST"
         >
           <input
-            placeholder="Phone Whatsapp Number"
+            placeholder="WhatsApp Phone Number"
             required=""
             type="text"
             value={phoneNumber}
@@ -115,7 +101,7 @@ export default function Form() {
           />
           <button
             type="submit"
-            className="inline-flex lg:w-40 w-full items-center justify-center h-12 px-0 font-bold tracking-wide text-white transition duration-200 rounded shadow-md bg-green"
+            className="inline-flex items-center justify-center w-full h-12 px-0 font-bold tracking-wide text-white transition duration-200 rounded shadow-md bg-green"
           >
             Join Waitlist
           </button>
